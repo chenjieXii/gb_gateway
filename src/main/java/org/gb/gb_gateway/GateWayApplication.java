77466622af2e2b9e2f91596bd3e056df34bf9c9c
chenjieXii <chenbaiwan@88.com>
2023-04-09 23:23:54 +0800

    feat:初始化提交

---
+++ src/main/java/org/gb/gb_gateway/GateWayApplication.java
@@ -0,0 +1,20 @@
+package org.gb.gb_gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author HouYouWeng
+ * @ProjectName gb_gateway
+ * @Description TODO
+ * @time 2023/4/9 22:09
+ */
+
+
+@SpringBootApplication
+public class GateWayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GateWayApplication.class);
+    }
+}
